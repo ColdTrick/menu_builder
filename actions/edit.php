9799d495aa0da81247795a66be0829bd736ec95b
Matt Beckett <beck24@gmail.com>
2013-01-26 17:35:18 -0700

    adds settings to enable/disable htmlawed filtering of url and title

--- actions/edit.php
+++ actions/edit.php
@@ -1,7 +1,11 @@
 <?php 
+	$filter = true;
+	if (elgg_get_plugin_setting('htmlawed_filter', 'menu_builder') == 'no') {
+	  $filter = false;
+	}
 
-	$title = get_input("title");
-	$url = get_input("url");
+	$title = get_input("title", '', $filter);
+	$url = get_input("url", '', $filter);
 	$target = get_input("target");
 	$access_id = (int) get_input("access_id", ACCESS_DEFAULT);
 	$parent_guid = (int) get_input("parent_guid", 0);
---
+++ views/default/plugins/menu_builder/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+echo elgg_echo('menu_builder:htmlawed:filter') . '<br>';
+
+echo elgg_view('input/dropdown', array(
+	'name' => 'params[htmlawed_filter]',
+	'value' => $vars['entity']->htmlawed_filter ? $vars['entity']->htmlawed_filter : 'yes',
+	'options_values' => array(
+		'yes' => elgg_echo('option:yes'),
+		'no' => elgg_echo('option:no')
+	)
+));
+
+echo '<br><br>';
