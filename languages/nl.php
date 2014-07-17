36f86fcb0a5998cba4912eeb58efe3d5b02fb47f
Jerome Bakker <jeabakker@coldtrick.com>
2014-07-17 09:35:43 +0200

    dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,5 +1,30 @@
 <?php
+/**
+ * This file was created by Translation Editor v2.0
+ * On 2014-07-17 09:35
+ */
+
 $language = array (
+  'menu_builder:export:menu' => 'Exporteer Site Menu',
+  'menu_builder:import:menu' => 'Importeer Site menu',
+  'menu_builder:admin:menu:list' => 'Beheersbare menu\'s',
+  'menu_builder:admin:menu:add' => 'Nieuw menu',
+  'menu_builder:admin:menu:add:internal_name' => 'Interne menu naam',
+  'menu_builder:admin:menu:delete' => 'Verwijder menu',
+  'menu_builder:appearance:menu_items:disabled' => 'Deze pagina is uitgeschakeld door Menu Builder, welke de configuratie van het site menu heeft overgenomen',
+  'menu_builder:add:form:priority' => 'Prioriteit',
+  'menu_builder:add:action:tokens' => 'Actie',
+  'admin:menu_builder' => 'Menu Builder',
+  'admin:menu_builder:import' => 'Import',
+  'menu_builder:import:warning:title' => 'WAARSCHUWING',
+  'menu_builder:import:warning' => 'Het importeren van het site menu overschrijft het huidige menu, dit is niet ongedaan te maken. Ga verder op eigen risico.<br><br>Na het importeren controleer de links op hard-coded urls, aangezien deze nog naar het verkeerde domein kunnen wijzen als je geen gebruik hebt gemaakt van het [wwwroot] token, of als je importeert vanaf een systeem in een subfolder of importeert naar een systeem in een subfolder',
+  'menu_builder:import:help' => 'Upload een bestand van een geëxporteerd menu',
+  'menu_builder:actions:export:error:empty' => 'Er zijn geen menu items beschikbaar om te exporteren',
+  'menu_builder:actions:import:error:invalid:filetype' => 'Ongeldig bestandstype',
+  'menu_builder:actions:import:error:upload' => 'Er is een fout opgetreden tijdens de upload',
+  'menu_builder:actions:import:error:invalid:content' => 'Er is een probleem met de inhoud van het bestand',
+  'menu_builder:actions:import:complete' => 'Het menu is geïmporteerd',
+  'menu_builder:htmlawed:filter' => 'Filter url en titels middels HTMLawed?',
   'menu_builder:toggle_context' => 'Wissel weergave',
   'menu_builder:toggle_context:normal_user' => 'Bekijk het menu als gewone gebruiker',
   'menu_builder:toggle_context:logged_out' => 'Bekijk het menu als afgemelde gebruiker',
@@ -12,9 +37,9 @@ $language = array (
   'menu_builder:edit_mode:add' => 'Klik om een nieuw menu item toe te voegen',
   'menu_builder:add:title' => 'Nieuw menu item',
   'menu_builder:add:form:url' => 'URL',
-  'menu_builder:add:form:target' => "Venster",
-  'menu_builder:add:form:target:self' => "Zelfde Venster",
-  'menu_builder:add:form:target:blank' => "Nieuw Venster",
+  'menu_builder:add:form:target' => 'Venster',
+  'menu_builder:add:form:target:self' => 'Zelfde Venster',
+  'menu_builder:add:form:target:blank' => 'Nieuw Venster',
   'menu_builder:add:form:parent' => 'Hoofd menu item',
   'menu_builder:add:form:parent:toplevel' => 'Toplevel menu item',
   'menu_builder:add:form:access' => 'Wie kan dit menu item zien?',
