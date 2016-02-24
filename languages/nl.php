751b9b820e395e7f7066baeaea4b82adef5dc6d6
Jeroen Dalsem <jdalsem@coldtrick.com>
2016-02-24 11:59:47 +0100

    changed: updated the dutch translation

--- languages/nl.php
+++ languages/nl.php
@@ -1,51 +1,37 @@
 <?php
 /**
- * This file was created by Translation Editor v2.0
- * On 2014-07-17 09:35
+ * This file was created by Translation Editor v4.0
+ * On 2016-02-24 11:59
  */
 
-$language = array (
+return array (
+  'menu_builder:import:title' => 'Importeer menu items voor %s',
+  'menu_builder:actions:missing_name' => 'Ontbrekende menu naam',
+  'menu_builder:settings:regen_site_menu:button' => 'Opnieuw aanmaken!',
+  'menu_builder:settings:regen_site_menu' => 'Met deze link kun je er voor kiezen om het site menu opnieuw te genereren. Dit zal het bestaande site menu overschrijven met de standaard geregistreerde menu items.',
+  'menu_builder:admin:menu:placeholder' => 'Enkel aanwezig voor sortering',
   'menu_builder:admin:menu:list' => 'Beheersbare menu\'s',
   'menu_builder:admin:menu:add' => 'Nieuw menu',
   'menu_builder:admin:menu:add:internal_name' => 'Interne menu naam',
-  'menu_builder:admin:menu:delete' => 'Verwijder menu',
-  'menu_builder:appearance:menu_items:disabled' => 'Deze pagina is uitgeschakeld door Menu Builder, welke de configuratie van het site menu heeft overgenomen',
-  'menu_builder:add:form:priority' => 'Prioriteit',
   'menu_builder:add:action:tokens' => 'Actie',
-  'admin:menu_builder' => 'Menu Builder',
-  'menu_builder:import:warning:title' => 'WAARSCHUWING',
   'menu_builder:import:warning' => 'Het importeren van het site menu overschrijft het huidige menu, dit is niet ongedaan te maken. Ga verder op eigen risico.<br><br>Na het importeren controleer de links op hard-coded urls, aangezien deze nog naar het verkeerde domein kunnen wijzen als je geen gebruik hebt gemaakt van relatieve links, of als je importeert vanaf een systeem in een subfolder of importeert naar een systeem in een subfolder',
   'menu_builder:import:help' => 'Upload een bestand van een geëxporteerd menu',
   'menu_builder:actions:export:error:empty' => 'Er zijn geen menu items beschikbaar om te exporteren',
-  'menu_builder:actions:import:error:invalid:filetype' => 'Ongeldig bestandstype',
   'menu_builder:actions:import:error:upload' => 'Er is een fout opgetreden tijdens de upload',
   'menu_builder:actions:import:error:invalid:content' => 'Er is een probleem met de inhoud van het bestand',
   'menu_builder:actions:import:complete' => 'Het menu is geïmporteerd',
   'menu_builder:settings:htmlawed:filter' => 'Filter url en titels middels HTMLawed?',
-  'menu_builder:toggle_context' => 'Wissel weergave',
-  'menu_builder:toggle_context:normal_user' => 'Bekijk het menu als gewone gebruiker',
-  'menu_builder:toggle_context:logged_out' => 'Bekijk het menu als afgemelde gebruiker',
-  'menu_builder:toggle_context:all' => 'Bekijk alle menu items',
-  'menu_builder:toggle_context:default' => 'Bekijk het menu als admin',
   'menu_builder' => 'Menu Builder',
   'item:object:menu_builder_menu_item' => 'Menu Builder item',
-  'menu_builder:edit_mode:off' => 'Normale weergave',
-  'menu_builder:edit_mode:on' => 'Bewerk weergave',
   'menu_builder:edit_mode:add' => 'Klik om een nieuw menu item toe te voegen',
-  'menu_builder:add:title' => 'Nieuw menu item',
   'menu_builder:add:form:url' => 'URL',
   'menu_builder:add:form:target' => 'Venster',
   'menu_builder:add:form:target:self' => 'Zelfde venster',
   'menu_builder:add:form:target:blank' => 'Nieuw venster',
   'menu_builder:add:form:parent' => 'Hoofd menu item',
-  'menu_builder:add:form:parent:toplevel' => 'Toplevel menu item',
   'menu_builder:add:form:access' => 'Wie kan dit menu item zien?',
   'menu_builder:add:access:admin_only' => 'Enkel admins',
   'menu_builder:actions:edit:error:input' => 'Foutieve invoer',
-  'menu_builder:actions:edit:error:create' => 'Er is een fout opgetreden, probeer het opnieuw',
-  'menu_builder:actions:edit:error:save' => 'Er is een fout opgetreden, probeer het opnieuw',
   'menu_builder:actions:edit:success' => 'Menu item opgeslagen',
   'menu_builder:actions:delete:success' => 'Menu item verwijderd',
-  'LOGGED_OUT' => 'Afgemelde gebruikers',
 );
-add_translation("nl", $language);
