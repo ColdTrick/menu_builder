069c2ea9fab9ac138d62811bf13f8ec51d78f60d
Jeroen Dalsem <jdalsem@coldtrick.com>
2018-02-20 11:03:57 +0100

    chore: moved functions into start file

--- start.php
+++ start.php
@@ -2,8 +2,6 @@
 
 define('MENU_BUILDER_ACCESS_LOGGED_OUT', -5);
 
-require_once(dirname(__FILE__) . '/lib/functions.php');
-
 // register default Elgg events
 elgg_register_event_handler('init', 'system', 'menu_builder_init');
 
@@ -52,5 +50,35 @@ function menu_builder_ready() {
 			elgg_register_plugin_hook_handler('view', "navigation/menu/{$menu_name}", '\ColdTrick\MenuBuilder\MenuHooks::afterViewMenu', 9999);
 		}
 	}
+}
+
+/**
+ * Returns an array of all the menu names that are managed by menu_builder
+ *
+ * @return array
+ */
+function menu_builder_get_managed_menus() {
+	static $result;
+	if (isset($result)) {
+		return $result;
+	}
+	$result = json_decode(elgg_get_plugin_setting('menu_names', 'menu_builder'), true);
+	return (array) $result;
+}
+
+/**
+ * Checks if a menu is a managed menu
+ *
+ * @param string $menu_name name of the menu item to check
+ *
+ * @return bool
+ */
+function menu_builder_is_managed_menu($menu_name) {
+	if (empty($menu_name)) {
+		return false;
+	}
+	
+	$menus = menu_builder_get_managed_menus();
+	return in_array($menu_name, $menus);
 }
 	
