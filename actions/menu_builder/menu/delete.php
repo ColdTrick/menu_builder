3f0d393d67db9f98e8f32c1de443ff181d89d251
Jeroen Dalsem <jdalsem@coldtrick.com>
2023-05-09 14:03:39 +0200

    chore: validate action input

--- actions/menu_builder/menu/delete.php
+++ actions/menu_builder/menu/delete.php
@@ -2,5 +2,9 @@
 
 $menu_name = get_input('menu_name');
 
+if (elgg_is_empty($menu_name)) {
+	return elgg_error_response(elgg_echo('error:missing_data'));
+}
+
 $menu = new \ColdTrick\MenuBuilder\Menu($menu_name);
 $menu->delete();
--- actions/menu_builder/menu_item/delete.php
+++ actions/menu_builder/menu_item/delete.php
@@ -3,6 +3,10 @@
 $name = get_input('item_name');
 $menu_name = get_input('menu_name');
 
+if (elgg_is_empty($name) || elgg_is_empty($menu_name)) {
+	return elgg_error_response(elgg_echo('error:missing_data'));
+}
+
 $menu = new \ColdTrick\MenuBuilder\Menu($menu_name);
 $menu_items = $menu->getMenuConfig();
 if (empty($menu_items)) {
