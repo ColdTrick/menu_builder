dcdb9d06231021cef5be42f32667011d45e14e92
Jerôme Bakker <jeabakker@coldtrick.com>
2023-05-17 15:37:58 +0200

    chore: updated translations

--- languages/nl.php
+++ languages/nl.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * This file was created by Translation Editor v7.0.4
- * On 2019-08-28 10:36
+ * This file was created by Translation Editor v11.1
+ * On 2023-05-17 14:48
  */
 
 return array (
